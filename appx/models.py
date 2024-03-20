6e0a6736a77be96885f28b300250443f1abd0b49
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-03-20 18:55:33 +0530

    Update models.py

--- appx/models.py
+++ appx/models.py
@@ -1,9 +1,29 @@
+# from django.db import models
+# from autoslug import AutoSlugField
+
+
+# class blogx(models.Model):
+#     blog_id = models.IntegerField(primary_key=True, null=False)
+#     blog_name = models.CharField(max_length=100)
+#     blog_code = models.TextField(max_length=10000)
+#     blog_desc = models.TextField(max_length=10000)
+    
+
+#     slugx = AutoSlugField(populate_from='blog_name',unique=True, null=True , default=None )
+
+# class comment(models.Model):
+#     cum_id = models.ForeignKey(blogx,on_delete=models.CASCADE)
+#     text = models.TextField(max_length=200)
+
+
+
+
 from django.db import models
 from autoslug import AutoSlugField
 
 
 class blogx(models.Model):
-    blog_id = models.IntegerField(primary_key=True, null=False)
+    blog_id = models.AutoField(primary_key=True)
     blog_name = models.CharField(max_length=100)
     blog_code = models.TextField(max_length=10000)
     blog_desc = models.TextField(max_length=10000)
