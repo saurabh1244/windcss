7f21ca589a41f68c8ce20a7c4263a4430d040492
Saurabh chandra <126794525+saurabh1244@users.noreply.github.com>
2024-03-20 18:56:10 +0530

    Update views.py

--- appx/views.py
+++ appx/views.py
@@ -1,3 +1,117 @@
+# from django.shortcuts import render , HttpResponse , redirect
+# from .models import blogx ,comment
+# from django.contrib.auth.models import User
+# from django.contrib.auth.hashers import make_password
+# from django.db.models import Q
+# from django.contrib.auth import authenticate , login , logout
+# from django.contrib.auth.decorators import login_required
+
+
+# def register_page(request):
+#     if request.method == 'POST':
+#         fname = request.POST.get('fname')
+#         lname = request.POST.get('lname')
+#         username = request.POST.get('uname')
+#         email = request.POST.get('email')
+#         passx = request.POST.get('passx')
+
+#         print(fname, lname, email,passx ,username )
+
+
+
+#         obj = User.objects.create(username = username , first_name=fname,last_name=lname,email=email )
+#         obj.set_password(passx)
+#         obj.save()
+
+#         print(fname, lname, email,passx ,username )
+
+#     return render(request, 'register.html')
+
+
+# def login_page(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('uname')
+#         passx = request.POST['passx']
+
+#         if User.objects.filter(username=username).exists():
+#             user_obj = authenticate(username=username,password=passx)
+#             login(request,user_obj)
+#             print('login sucess using ...')
+#             print(username,passx)
+#             return redirect('/')
+
+#         else:
+#             return HttpResponse('any error occuredin if statement')
+
+#     return render(request , 'login.html')
+
+# def logout_page(request):
+#     logout(request)
+#     return redirect('/')
+
+# def page(request):
+#     obj = blogx.objects.all()
+#     context = {
+#         'datas': obj
+#     }
+#     return render(request , 'index.html',context)
+    
+
+# def home(request):
+#        return render(request, 'home.html')
+
+
+
+# def post(request,slug):
+#     cum_obj = None
+#     if request.method == 'POST':
+#         commentx = request.POST['comment']
+#         print(comment)
+
+#         obj = blogx.objects.get(slugx=slug)
+
+#         cum_obj = comment.objects.create(cum_id=obj,text=commentx)
+#         cum_obj.save()
+#         print("succefully added comments...")
+
+
+
+#     obj = blogx.objects.get(slugx=slug)
+#     comments = comment.objects.filter(cum_id=obj).order_by('-id')
+
+#     context = {
+#         'html':obj,
+#         'new_comment':cum_obj,
+#         'comments':comments
+#     }
+
+#     return render(request , 'datax.html',context)
+
+# @login_required(login_url='/login/')
+# def post_user(request):
+#     if request.method == 'POST':
+#         blog_id = request.POST['blog_id']
+#         blog_name = request.POST['blog_name']
+#         blog_code = request.POST['blog_code']
+#         blog_desc = request.POST['blog_desc']
+
+#         obj = blogx.objects.create(blog_id=blog_id, blog_name=blog_name, blog_code=blog_code, blog_desc=blog_desc)
+#         obj.save()
+#         return HttpResponse('add blog succesfully..')
+
+
+#     return render(request , 'post_user.html')
+
+
+
+
+
+
+
+
+
+
+
 from django.shortcuts import render , HttpResponse , redirect
 from .models import blogx ,comment
 from django.contrib.auth.models import User
@@ -22,6 +136,7 @@ def register_page(request):
         obj = User.objects.create(username = username , first_name=fname,last_name=lname,email=email )
         obj.set_password(passx)
         obj.save()
+        return redirect('/')
 
         print(fname, lname, email,passx ,username )
 
@@ -57,8 +172,25 @@ def page(request):
     return render(request , 'index.html',context)
     
 
+# def home(request):
+#        return render(request, 'home.html')
+
 def home(request):
-       return render(request, 'home.html')
+       if request.method == 'POST':
+        search_query = request.POST.get('search')
+        objs = blogx.objects.filter(
+            Q(blog_name__icontains=search_query)   
+        )
+
+       elif request.method is None:
+           objs = blogx.objects.all()
+           
+
+       else:
+        # If no search query is provided, display all room_rent objects
+        objs = blogx.objects.all().order_by('-blog_id')
+
+       return render(request, 'home.html', {'datas': objs})
 
 
 
@@ -90,14 +222,15 @@ def post(request,slug):
 @login_required(login_url='/login/')
 def post_user(request):
     if request.method == 'POST':
-        blog_id = request.POST['blog_id']
+        # blog_id = request.POST['blog_id']
         blog_name = request.POST['blog_name']
         blog_code = request.POST['blog_code']
-        blog_desc = request.POST['blog_desc']
+        user_html_data = request.POST['blog_desc']
+        blog_desc = f'<xmp>\n{user_html_data}\n</xmp>'
 
-        obj = blogx.objects.create(blog_id=blog_id, blog_name=blog_name, blog_code=blog_code, blog_desc=blog_desc)
+        obj = blogx.objects.create( blog_name=blog_name, blog_code=blog_code, blog_desc=blog_desc)
         obj.save()
-        return HttpResponse('add blog succesfully..')
+        return redirect('/')
 
 
     return render(request , 'post_user.html')
